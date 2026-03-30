c874140a94223ed98de873a6f92455b2b803b012
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-03-30 13:37:57 +0530

    fixed tailwind issue

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      Hi from APP
-      
-    </div>
+    <main className='h-screen w-full bg-gray-950 text-white flex items-center justify-center'>
+      Sachin
+
+      <h1>Saad</h1>
+    </main>
   )
 }
 
